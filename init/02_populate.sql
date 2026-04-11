9d4be9d8b5b06912c21d935284c4422ebbb92ca6
João Ventura <venturajpo@gmail.com>
2026-04-11 17:12:35 -0300

    refactor: criado scripts de inicialização do banco
    movido e renomeado schema.sql para 01_schema.sql
    
    criado 02_populate.sql para preencher dados fantasia de funcionário

---
+++ init/02_populate.sql
@@ -0,0 +1,23 @@
+USE estoque_roupa;
+
+INSERT INTO funcionario (matricula, nome) VALUES
+ ('5890949', 'Alfredo Caetano')
+,('6197335', 'Giovane Moura')
+,('5218546', 'Norberto Carvalho')
+,('8573207', 'Cleiton Dias')
+,('9875402', 'Rogério Brandão')
+,('6202213', 'André Barroso')
+,('1120851', 'Mateus Furtado')
+,('6962353', 'Gustavo Padilha')
+,('6178100', 'Arnaldo Reis')
+,('4291009', 'Julio Tavares')
+,('1193513', 'Poliana Costa')
+,('5546206', 'Olívia Higashi')
+,('0571467', 'Camila Justo')
+,('9533578', 'Cristina Martins')
+,('1166648', 'Maiara Salgado')
+,('2748750', 'Zilda Cruz')
+,('5359872', 'Vânia Dutra')
+,('4322021', 'Sabrina Kimura')
+,('7953154', 'Clara Barroso')
+,('3044835', 'Mariana Nogueira');
